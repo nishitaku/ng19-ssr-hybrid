25592083f36314bedf8cc1ddadd49011fbd3d987
nishitaku <29690066+nishitaku@users.noreply.github.com>
2024-11-26 10:14:31 +0900

    feat: add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,41 @@
 import { Routes } from '@angular/router';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { BlogPageComponent } from './pages/blog-page/blog-page.component';
+import { PostPageComponent } from './pages/post-page/post-page.component';
+import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
+import { SettingPageComponent } from './pages/setting-page/setting-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/about',
+  },
+  {
+    path: 'about',
+    component: AboutPageComponent,
+  },
+  {
+    path: 'profile',
+    component: ProfilePageComponent,
+    children: [
+      {
+        path: 'setting',
+        component: SettingPageComponent,
+      },
+    ],
+  },
+  {
+    path: 'blog',
+    children: [
+      {
+        path: '',
+        component: BlogPageComponent,
+      },
+      {
+        path: ':id',
+        component: PostPageComponent,
+      },
+    ],
+  },
+];
