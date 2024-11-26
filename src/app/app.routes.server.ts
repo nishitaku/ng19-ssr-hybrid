5610019e25e252a469662848388ee28ecd5f6883
nishitaku <29690066+nishitaku@users.noreply.github.com>
2024-11-26 17:57:27 +0900

    feat: implement app.routes.server.ts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,35 @@
+import { inject } from '@angular/core';
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { BlogService } from './services/blog.service';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'about',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'profile',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'setting',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'blog',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'blog/:id',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      const blogService = inject(BlogService);
+      const ids = blogService.fetchBlogIds();
+      return ids().map((id) => ({ id }));
+    },
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
