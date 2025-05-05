2c7904a763eea0b893d24caae45730c143c89975
nishitaku <29690066+nishitaku@users.noreply.github.com>
2025-05-05 16:03:53 +0900

    feat: provide Auth0

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,14 +1,23 @@
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, mergeApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { provideServerRoutesConfig } from '@angular/ssr';
+import { AuthService } from '@auth0/auth0-angular';
+import { of } from 'rxjs';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
 
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
-    provideServerRoutesConfig(serverRoutes)
-  ]
+    provideServerRoutesConfig(serverRoutes),
+    // SSRでは認証しないのでモックする
+    {
+      provide: AuthService,
+      useValue: {
+        isAuthenticated$: of(),
+      },
+    },
+  ],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
