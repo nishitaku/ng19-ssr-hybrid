4c33b12e8a209eac2485291ede6d247b161f28eb
nishitaku <29690066+nishitaku@users.noreply.github.com>
2025-05-05 16:04:24 +0900

    feat: implement main layout with navigation and authentication handling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,6 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from '@auth0/auth0-angular';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { BlogPageComponent } from './pages/blog-page/blog-page.component';
 import { PostPageComponent } from './pages/post-page/post-page.component';
@@ -8,31 +10,38 @@ import { SettingPageComponent } from './pages/setting-page/setting-page.componen
 export const routes: Routes = [
   {
     path: '',
-    pathMatch: 'full',
-    redirectTo: '/blog',
-  },
-  {
-    path: 'about',
-    component: AboutPageComponent,
-  },
-  {
-    path: 'profile',
-    component: ProfilePageComponent,
-  },
-  {
-    path: 'setting',
-    component: SettingPageComponent,
-  },
-  {
-    path: 'blog',
+    component: MainLayoutComponent,
     children: [
       {
         path: '',
-        component: BlogPageComponent,
+        pathMatch: 'full',
+        redirectTo: 'blog',
       },
       {
-        path: ':id',
-        component: PostPageComponent,
+        path: 'about',
+        component: AboutPageComponent,
+      },
+      {
+        path: 'profile',
+        component: ProfilePageComponent,
+      },
+      {
+        path: 'setting',
+        canActivate: [AuthGuard],
+        component: SettingPageComponent,
+      },
+      {
+        path: 'blog',
+        children: [
+          {
+            path: '',
+            component: BlogPageComponent,
+          },
+          {
+            path: ':id',
+            component: PostPageComponent,
+          },
+        ],
       },
     ],
   },
--- src/app/layout/main-layout/main-layout.component.ts
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './main-layout.component.html',
-  styleUrl: './main-layout.component.css'
+  styleUrl: './main-layout.component.css',
 })
 export class MainLayoutComponent {
+  private readonly authService = inject(AuthService);
 
+  isAuthenticated = toSignal(this.authService.isAuthenticated$);
+
+  settingPageLinkText = computed(() =>
+    this.isAuthenticated() ? 'Setting' : 'Login'
+  );
 }
